78b2e89bbec7022d92870c0801164698800076c0
containerscrew <131241415+containerscrew@users.noreply.github.com>
2024-05-31 15:25:09 +0200

    Implement SSM start-session command

--- src/cloudsnake/ssm.py
+++ src/cloudsnake/ssm.py
@@ -1,4 +1,7 @@
+import errno
+import json
 import logging
+import subprocess
 
 from botocore.config import Config
 
@@ -10,19 +13,59 @@ ERROR_MESSAGE = (
     "session-manager-plugin-not-found",
 )
 
-
 class SSM:
-    def __init__(self, session, filters=None, query=None, output=None):
+    def __init__(self, session, *args, **kwargs):
         self.log = logging.getLogger("cloudsnake")
-        self.query = query
-        self.output = output
-        self.filters = filters
-        self.ec2_client = self.create_ssm_client(session)
+        self.ssm_client = self.create_ssm_client(session)
+        self.args = args
+        self.kwargs = kwargs
 
     @staticmethod
     def create_ssm_client(session):
         config = Config(retries={"max_attempts": 10, "mode": "standard"})
         return session.client("ssm", config=config)
+
+
+class StartSessionWrapper(SSM):
+    def __init__(self, session, session_response_output=None, *args, **kwargs):
+        # Inheriting the properties of parent class
+        super().__init__(session, *args, **kwargs)
+        self.session_response_output = session_response_output
+
+    def start_session_response(self):
+        target = self.kwargs.get("target")
+        reason = self.kwargs.get("reason")
+        response = self.ssm_client.start_session(
+            Target=target,
+            Reason=reason,
+        )
+        self.session_response_output = response
+
+    def start_session(self, region: str, profile: str):
+        self.start_session_response()
+        target = self.kwargs.get("target")
+        try:
+            subprocess.check_call(
+                [
+                    "session-manager-plugin",
+                    json.dumps(self.session_response_output),
+                    region,
+                    "StartSession",
+                    profile,
+                    json.dumps(dict(Target=target)),
+                    f"https://ssm.{region}.amazonaws.com",
+                ]
+            )
+            return 0
+        except OSError as ex:
+            if ex.errno == errno.ENOENT:
+                self.log.error("SessionManagerPlugin is not present", exc_info=True)
+                print("SessionManagerPlugin is not present")
+                self.ssm_client.terminate_session(
+                    SessionId=self.session_response_output["SessionId"]
+                )
+                raise ValueError("".join(ERROR_MESSAGE))
+
 
 
 # class SSMSession:
