dfce684b81a8bc20b722874c885abd8c62094d94
containerscrew <131241415+containerscrew@users.noreply.github.com>
2024-05-31 15:25:09 +0200

    chore: auto-fix non-conventional commit - Implement SSM start-session command

--- src/cloudsnake/cli.py
+++ src/cloudsnake/cli.py
@@ -10,6 +10,7 @@ from typing_extensions import List
 from cloudsnake.session import SessionWrapper
 from cloudsnake.logger import init_logger
 from cloudsnake.ec2 import InstanceWrapper
+from cloudsnake.ssm import StartSessionWrapper
 
 
 class OutputMode(str, Enum):
@@ -39,6 +40,8 @@ app.add_typer(ec2, name="ec2", help="Manage ec2 operations")
 @dataclass
 class Common:
     session: boto3.Session
+    profile: str
+    region: str
 
 
 @ec2.command(
@@ -46,37 +49,46 @@ class Common:
     help="Describe EC2 instances data with filters and query as a parameter",
 )
 def describe_instances(
-    ctx: typer.Context,
-    filters: Annotated[
-        List[str], typer.Option(help="Filters for EC2 instances in Name=Value format")
-    ] = None,
-    query: Annotated[str, typer.Option(help="Query to format the output")] = None,
-    output: Annotated[
-        OutputMode,
-        typer.Option(help="Output mode", case_sensitive=True),
-    ] = OutputMode.json,
+        ctx: typer.Context,
+        filters: Annotated[
+            List[str], typer.Option(help="Filters for EC2 instances in Name=Value format")
+        ] = None,
+        query: Annotated[str, typer.Option(help="Query to format the output")] = None,
+        output: Annotated[
+            OutputMode,
+            typer.Option(help="Output mode", case_sensitive=True),
+        ] = OutputMode.json,
 ):
     """Invoke ec2 describe-instances"""
     instances = InstanceWrapper(ctx.obj.session, filters=filters, query=query, output=output)
     instances.print_console()
 
 
+@ssm.command("start-session", help="Start session with the given target id")
+def start_session(ctx: typer.Context,
+                  target: str = typer.Option(help="Target id of the instance"),
+                  reason: Annotated[str, typer.Option(help="Reason of the connection")] = "default-connection",
+                  ):
+    ssm_session = StartSessionWrapper(ctx.obj.session, target=target, reason=reason)
+    ssm_session.start_session(ctx.obj.region, ctx.obj.profile)
+
+
 @app.callback()
 def entrypoint(
-    ctx: typer.Context,
-    profile: Annotated[
-        str, typer.Option(help="AWS profile to use", show_default=True)
-    ] = os.getenv("AWS_PROFILE"),
-    log_level: Annotated[
-        LoggingLevel,
-        typer.Option(
-            help="Logging level for the app custom code and boto3",
-            case_sensitive=False,
-        ),
-    ] = LoggingLevel.WARNING,
-    region: Annotated[
-        str, typer.Option(help="AWS region", show_default=True)
-    ] = "eu-west-1",
+        ctx: typer.Context,
+        profile: Annotated[
+            str, typer.Option(help="AWS profile to use", show_default=True)
+        ] = os.getenv("AWS_PROFILE"),
+        log_level: Annotated[
+            LoggingLevel,
+            typer.Option(
+                help="Logging level for the app custom code and boto3",
+                case_sensitive=False,
+            ),
+        ] = LoggingLevel.INFO,
+        region: Annotated[
+            str, typer.Option(help="AWS region", show_default=True)
+        ] = "eu-west-1",
 ):
     """
     cloudsnake is an AWS cli wrapper with beautiful TUI using rich, typer and textual. It does not implement all the
@@ -85,6 +97,6 @@ def entrypoint(
     Example: cloudsnake ec2 describe-instances
     """
     session = SessionWrapper(profile, region).with_local_session()
-    ctx.obj = Common(session)
+    ctx.obj = Common(session, profile, region)
     logger = init_logger(log_level.value)
     logger.info("Starting cloudsnake 🐍☁")
